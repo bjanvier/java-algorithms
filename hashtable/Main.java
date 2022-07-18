b3011cf650527c086bcb55ee86d076e7af1bbe5a
=bjanvier <=>
2022-07-18 01:08:37 -0400

    Added SuppressWarnings notation

--- hashtable/Main.java
+++ hashtable/Main.java
@@ -1,15 +1,14 @@
 package hashtable;
 
+import java.util.List;
+
 public class Main {
-
     public static void main(String[] args) {
-        HashTable<Object> table = new HashTable<>();
-        Object[] arr = { 1243, 345, true, false, "W", "hahas", "LOL", "Jesus is Our Savior" };
-
+        HashTable table = new HashTable();
+        Object[] arr = { 1243, 345, true, false, "Janvier", "Zagabe",
+                "Either Coding/Programming, or Soccer, Or nothing",
+                "Jesus is Our Savior" };
         Object[] arr1 = table.linearProbing(arr);
-
-        // System.out.println(arr.length);
-        // System.out.println(table.linearProbing(arr).length);
 
         System.out.println("Before converting to hash table using liner probing");
         for (int i = 0; i < arr.length; i++) {
@@ -19,16 +18,18 @@ public class Main {
 
         System.out.println("\n\nAfter converting to hash table using liner probing");
         for (int i = 0; i < arr1.length; i++) {
-            System.out.print(arr1[i] + " ");
+            System.out.print(arr1[i] + ", ");
         }
-        // System.out.println(arr.length);
-        // System.out.println(table.compress("VII", arr.length));
-        // System.out.println(table.compress(33, arr.length));
+        System.out.println();
 
-        // Same hash codes
-        // System.out.println(table.generateHashCode("VII"));
-        // System.out.println(table.generateHashCode("Ugh"));
-        // System.out.println("Ugh".hashCode());
-
+        List<Object>[] lBuckets = table.separateChaining(arr);
+        for (int i = 0; i < lBuckets.length; i++) {
+            if (lBuckets[i] != null) {
+                for (int j = 0; j < lBuckets[i].size(); j++) {
+                    System.out.print(lBuckets[i].get(j) + ", ");
+                }
+            }
+        }
+        System.out.println();
     }
 }
