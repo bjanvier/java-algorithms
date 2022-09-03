49ee4c6581b1fec4ec94d996c1dbb78094ea2eb0
=bjanvier <=>
2022-09-03 15:16:01 -0400

    update Permutations

--- recursion/Main.java
+++ recursion/Main.java
@@ -11,13 +11,13 @@ public class Main {
     System.out.println("Permutations");
     System.out.println("inputs List => " + arr.size());
 
-    for (int i = 0; i < arr.size(); i++) {
-      System.out.println(arr.get(i));
-      for (int j = i + 1; j < arr.size(); j++) {
-        if (arr.get(i).equals(arr.get(j))) {
-          System.out.println("Duplicate element found " + arr.get(i));
-        }
-      }
-    }
+    // for (int i = 0; i < arr.size(); i++) {
+    // System.out.println(arr.get(i));
+    // for (int j = i + 1; j < arr.size(); j++) {
+    // if (arr.get(i).equals(arr.get(j))) {
+    // System.out.println("Duplicate element found " + arr.get(i));
+    // }
+    // }
+    // }
   }
 }
